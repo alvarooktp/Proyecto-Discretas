763b12b19ef08e76f2ad8d5489b6a75eda64d11f
alvarooktp <the_golden_mystery@hotmail.com>
2014-05-18 18:18:42 -0500

    Update ventanaprincipal.h

--- ventanaprincipal.h
+++ ventanaprincipal.h
@@ -2,18 +2,28 @@
 #define VENTANAPRINCIPAL_H
 
 #include <QMainWindow>
+#include <QPointer>
+#include <QMessageBox>
 
 namespace Ui {
 class VentanaPrincipal;
 }
 
+class QComboBox;
 class VentanaPrincipal : public QMainWindow
 {
     Q_OBJECT
+    QList<QPointer <QComboBox> > opciones;
+    QList <qintptr <QComboBox> > iopn;
 
 public:
     explicit VentanaPrincipal(QWidget *parent = 0);
     ~VentanaPrincipal();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::VentanaPrincipal *ui;
