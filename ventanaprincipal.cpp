bcf0d7bece069e54724331ad6e6f773efc2d5e1d
alvarooktp <the_golden_mystery@hotmail.com>
2014-05-18 18:18:13 -0500

    Update ventanaprincipal.cpp

--- ventanaprincipal.cpp
+++ ventanaprincipal.cpp
@@ -1,5 +1,6 @@
 #include "ventanaprincipal.h"
 #include "ui_ventanaprincipal.h"
+#include <QComboBox>
 
 VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,66 @@ VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
 VentanaPrincipal::~VentanaPrincipal()
 {
     delete ui;
+}
+
+void VentanaPrincipal::on_pushButton_clicked()
+{
+
+    int tam=0;
+    tam = (ui->lineEdit->text()).toInt();
+    ui->lineEdit->setDisabled(true);
+    ui->pushButton->setDisabled(true);
+
+
+    if(tam>=2 && tam<= 10)
+    {
+        ui->tableWidget->setEnabled(true);
+        ui->pushButton_2->setEnabled(true);
+
+        for(int i=0; i < tam ;i++){
+
+            const int ultima_fila = ui->tableWidget->rowCount();
+            ui->tableWidget->insertRow((ultima_fila));
+            const int ultima_columna = ui->tableWidget->columnCount();
+            ui->tableWidget->insertColumn((ultima_columna));
+
+        }
+
+        for(int i=0; i < tam ;i++){
+
+            for(int j=0; j < tam ;j++){
+                QPointer<QComboBox> mi_lista= new QComboBox(this);
+                QStringList valores;
+                valores<<"-Elejir opción-"
+                       <<"0"
+                       <<"1";
+                opciones.append(mi_lista);
+                mi_lista->addItems(valores);
+                ui->tableWidget->setCellWidget(i,j,mi_lista);
+            }
+           // break;
+        }
+
+    }else{
+        QMessageBox::information(this,"ERROR","Tamaño no valido.");
+    }
+}
+
+void VentanaPrincipal::on_pushButton_2_clicked()
+{
+    int tam= ui->lineEdit->text().toInt();
+    int matriz[tam][tam];
+    int a=99;
+    QTableWidgetItem *p;
+
+    for(int i=0; i < tam ;i++){
+        for(int j=0; j < tam ;j++){
+            p= ui->tableWidget->item(i,j);
+            matriz[i][j]= ;
+        }
+    }
+    //a=matriz[1][0];
+    //QString s=(QString)a;
+
+    //QMessageBox::information(this,"Matriz capturada",s);
 }
